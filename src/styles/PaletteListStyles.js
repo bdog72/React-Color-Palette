2d87635a6e70b420741a690bd520c865df3e3d2e
Brian Bycynski <high5bri@hotmail.com>
2019-05-12 10:05:26 -0400

    Finished lecture 205

---
+++ src/styles/PaletteListStyles.js
@@ -0,0 +1,30 @@
+export default {
+  root: {
+    // backgroundColor: 'blue',
+    background:
+      'linear-gradient(to bottom, #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%)',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  container: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    flexWrap: 'wrap'
+  },
+  nav: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between'
+  },
+  palettes: {
+    boxSizing: 'border-box',
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '5%'
+  }
+};
